70b9e4bc24f44c37a5485952b64ec7a03b9f5725
Albert Szabo <albertszabo@Alberts-MacBook-Pro.local>
2022-03-24 12:03:35 -0700

    Completed the first three tasks

--- index.js
+++ index.js
@@ -113,7 +113,14 @@ class Car {
 */
 
 class Lambdasian {
-  
+  constructor(props) {
+    this.name = props.name;
+    this.age = props.age;
+    this.location = props.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
